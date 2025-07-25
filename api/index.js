fa85a7625ad5d12bc77bc80f967dfbe965435ddd
omarhassan226 <oh.22697@gmail.com>
2025-07-25 18:58:02 +0300

    deploying

--- api/index.js
+++ api/index.js
@@ -4,15 +4,18 @@ const cors = require("cors");
 const connectDB = require("../src/configs/db");
 const bookRoutes = require("../src/routes/book.routes");
 
-const serverless = require("serverless-http");
-
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/books", bookRoutes);
 
-module.exports.handler = serverless(app);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
